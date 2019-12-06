956180df6ecab0fade5d7c75281a80e4ee60197e
Norman Jarvis <norman.jarvis@gmail.com>
2019-12-06 20:10:24 +0000

    Fixed the run.sh script to use the java system property

--- java/run.sh
+++ java/run.sh
@@ -9,7 +9,8 @@ if [ ! -z "$1" ]; then
 fi
 echo "Using serverone genesis path: ${SERVERONE_GENESIS_PATH}"
 
-#RUST_LOG=trace RUST_BACKTRACE=1 java -jar build/libs/indy-cloud-0.0.1-SNAPSHOT.jar > indy-cloud.server.output.log 2>&1 &
-RUST_LOG=trace RUST_BACKTRACE=1 java -Dserverone.genesis.path=${SERVERONE_GENESIS_PATH} -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=1044 -jar build/libs/indy-cloud-0.0.1-SNAPSHOT.jar > indy-cloud.server.output.log 2>&1 &
+RUST_LOG=trace RUST_BACKTRACE=1 java -Dserverone.genesis.path=${SERVERONE_GENESIS_PATH} -jar build/libs/indy-cloud-0.0.1-SNAPSHOT.jar > indy-cloud.server.output.log 2>&1 &
+#RUST_LOG=trace RUST_BACKTRACE=1 java -Dserverone.genesis.path=${SERVERONE_GENESIS_PATH} -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=1044 -jar build/libs/indy-cloud-0.0.1-SNAPSHOT.jar > indy-cloud.server.output.log 2>&1 &
+#RUST_LOG=trace RUST_BACKTRACE=1 java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=1044 -jar build/libs/indy-cloud-0.0.1-SNAPSHOT.jar > indy-cloud.server.output.log 2>&1 &
 
 echo $! > indy-cloud.pid
